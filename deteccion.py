ea4ed48051f936f7be3db88f3d3a27788f8ca559
irobledovega <57196561+irobledovega@users.noreply.github.com>
2020-03-20 11:44:21 -0700

    Update deteccion.py

--- deteccion.py
+++ deteccion.py
@@ -1,7 +1,12 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-input_img = cv.imread('prueba1.jpg',0)
+# Abre la imagen definida por el usuario
+try:
+  archivo = input(‘Nombre de la imagen a procesar: ‘)
+  input_img = cv.imread(archivo,0)
+except ValueError:
+  print(‘No se pudo abrir la imagen’)
 # Crea un objeto tipo FAST con parametros por defecto
 fast = cv.FastFeatureDetector_create()
 # Detecta los rasgos
