a9e9ce8d79766740984411877aa610a764044d88
irobledovega <57196561+irobledovega@users.noreply.github.com>
2020-04-01 13:36:23 -0700

    Update deteccion.py

--- deteccion.py
+++ deteccion.py
@@ -10,36 +10,36 @@ metodo = '1'
 
 # Funcion que se ejecuta al mover la barra de deslizamiento
 def detectar(x):
-  t=cv.getTrackbarPos('Threshold','Deteccion de Rasgos')
-
-  # Crea un objeto de acuerdo al metodo seleccionado
-  # y llama a la funcion para detectar los rasgos
-  if metodo=='1':
-    gftt = cv.GFTTDetector_create(0, 0.01, t, 3)
-    kp = gftt.detect(input_img,None)
-  elif metodo=='2':
-    # Crea un objeto tipo FAST
-    fast = cv.FastFeatureDetector_create(t)
-    # Detecta los rasgos
-    kp = fast.detect(input_img,None)
-  elif metodo=='3':
-    # Crea un objeto tipo AGAST
-    agast = cv.AgastFeatureDetector_create(t)
-    # Detecta los rasgos
-    kp = agast.detect(input_img,None)
-  else:
-    return
-  
-  #Dibuja los rasgos detectados sobre la imagen original
-  output_img = cv.drawKeypoints(input_img, kp, None, color=(255,0,0))
-  cv.imshow('Deteccion de Rasgos',output_img)
+    t=cv.getTrackbarPos('Threshold','Deteccion de Rasgos')
+    
+    # Crea un objeto de acuerdo al metodo seleccionado
+    # y llama a la funcion para detectar los rasgos
+    if metodo=='1':
+        gftt = cv.GFTTDetector_create(0, 0.01, t, 3)
+        kp = gftt.detect(input_img,None)
+    elif metodo=='2':
+        # Crea un objeto tipo FAST
+        fast = cv.FastFeatureDetector_create(t)
+        # Detecta los rasgos
+        kp = fast.detect(input_img,None)
+    elif metodo=='3':
+        # Crea un objeto tipo AGAST
+        agast = cv.AgastFeatureDetector_create(t)
+        # Detecta los rasgos
+        kp = agast.detect(input_img,None)
+    else:
+        return
+      
+    #Dibuja los rasgos detectados sobre la imagen original
+    output_img = cv.drawKeypoints(input_img, kp, None, color=(255,0,0))
+    cv.imshow('Deteccion de Rasgos',output_img)
 
 
 # Abre la imagen definida por el usuario
 archivo = input('Nombre de la imagen a procesar: ')
 while not os.path.isfile(archivo):
-  archivo = input('No se encontro el archivo.\nVuelve a teclear el nombre: ')
-  pass
+    archivo = input('No se encontro el archivo.\nVuelve a teclear el nombre: ')
+    pass
 input_img = cv.imread(archivo,0)
 
 #Permite al usuario seleccionar el metodo de deteccion de rasgos
