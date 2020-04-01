18c25c82bea77d9100898cd50d86e540c3aff061
irobledovega <57196561+irobledovega@users.noreply.github.com>
2020-04-01 13:40:17 -0700

    Update deteccion.py

--- deteccion.py
+++ deteccion.py
@@ -10,29 +10,29 @@ metodo = '1'
 
 # Funcion que se ejecuta al mover la barra de deslizamiento
 def detectar(x):
-    t=cv.getTrackbarPos('Threshold','Deteccion de Rasgos')
+    t = cv.getTrackbarPos('Threshold', 'Deteccion de Rasgos')
     
     # Crea un objeto de acuerdo al metodo seleccionado
     # y llama a la funcion para detectar los rasgos
-    if metodo=='1':
+    if metodo == '1':
         gftt = cv.GFTTDetector_create(0, 0.01, t, 3)
-        kp = gftt.detect(input_img,None)
-    elif metodo=='2':
+        kp = gftt.detect(input_img, None)
+    elif metodo == '2':
         # Crea un objeto tipo FAST
         fast = cv.FastFeatureDetector_create(t)
         # Detecta los rasgos
-        kp = fast.detect(input_img,None)
+        kp = fast.detect(input_img, None)
     elif metodo=='3':
         # Crea un objeto tipo AGAST
         agast = cv.AgastFeatureDetector_create(t)
         # Detecta los rasgos
-        kp = agast.detect(input_img,None)
+        kp = agast.detect(input_img, None)
     else:
         return
       
     #Dibuja los rasgos detectados sobre la imagen original
-    output_img = cv.drawKeypoints(input_img, kp, None, color=(255,0,0))
-    cv.imshow('Deteccion de Rasgos',output_img)
+    output_img = cv.drawKeypoints(input_img, kp, None, color = (255,0,0))
+    cv.imshow('Deteccion de Rasgos', output_img)
 
 
 # Abre la imagen definida por el usuario
@@ -40,7 +40,7 @@ archivo = input('Nombre de la imagen a procesar: ')
 while not os.path.isfile(archivo):
     archivo = input('No se encontro el archivo.\nVuelve a teclear el nombre: ')
     pass
-input_img = cv.imread(archivo,0)
+input_img = cv.imread(archivo, 0)
 
 #Permite al usuario seleccionar el metodo de deteccion de rasgos
 print('\nMetodos de Deteccion de Rasgos:\n(1)GFTT\n(2)FAST\n(3)AGAST\n(4)SIFT\n(5)SURF\n')
@@ -48,7 +48,7 @@ metodo = input('Selecciona el metodo: ')
 
 #Crea una ventana y muestra la imagen
 cv.namedWindow('Deteccion de Rasgos', cv.WINDOW_NORMAL)
-cv.createTrackbar('Threshold','Deteccion de Rasgos',10,100,detectar)
+cv.createTrackbar('Threshold','Deteccion de Rasgos', 10, 100, detectar)
 cv.imshow('Deteccion de Rasgos',input_img)
 
 #Ciclo que espera un evento
